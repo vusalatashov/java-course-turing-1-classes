23c80a45f99a1862a9f62e2e309af1a00f15dce0
vusalatashov <vusalatashov@gmail.com>
2025-04-27 12:12:08 +0400

    add Course and Student management functionality with repository, service, controller, and Liquibase changelogs

---
+++ spring-application-1/src/main/java/az/edu/turing/springapplication1/SpringApplication1Application.java
@@ -0,0 +1,13 @@
+package az.edu.turing.springapplication1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringApplication1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApplication1Application.class, args);
+    }
+}
