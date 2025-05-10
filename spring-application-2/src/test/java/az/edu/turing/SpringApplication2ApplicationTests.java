eda8647a6025e52d498dd586a046d6cb560396b5
vusalatashov <vusalatashov@gmail.com>
2025-05-10 12:20:30 +0400

    initialize Spring Boot application with basic configuration and testing setup

---
+++ spring-application-2/src/test/java/az/edu/turing/SpringApplication2ApplicationTests.java
@@ -0,0 +1,13 @@
+package az.edu.turing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApplication2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
